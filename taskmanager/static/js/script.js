7d6a4a3c9d8f4f95490dd80891cf9e42736bc97b
DeannaCarina <deannacarina@hotmail.com>
2021-10-05 10:30:29 +0000

    Add functionality to add tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -7,3 +7,16 @@ document.addEventListener('DOMContentLoaded', function() {
     let modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let dropdown = document.querySelectorAll('select');
+    M.FormSelect.init(dropdown);
+  });
