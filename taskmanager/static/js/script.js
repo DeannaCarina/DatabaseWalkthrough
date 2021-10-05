80e58336607b914d314c50fcee65657d0b366b43
DeannaCarina <deannacarina@hotmail.com>
2021-10-05 09:41:46 +0000

    Got modals up and running

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
+  });
